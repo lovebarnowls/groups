7b517b812a5aa76e7df2ec4da5573c6341baa894
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-01-09 10:24:10 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-sudo useradd saul
-sudo useradd luke
-sudo useradd joy
-sudo useradd judas
-sudo useradd pilate
-sudo useradd james
-sudo useradd grace
-sudo useradd jordan
-sudo useradd theresa
-sudo useradd phoebe
-sudo useradd peter
-sudo useradd john
-sudo passwd -d james
-sudo passwd -d judas
-sudo usermod -aG sudo judas
-sudo usermod -aG sudo james
-sudo chown joy /etc/passwd
+sudo useradd violin
+sudo useradd viola
+sudo useradd cello
+sudo useradd flute
+sudo useradd clarinet
+sudo useradd oboe
+sudo useradd bassoon
+sudo useradd trumpet
+sudo useradd trombone
+sudo useradd french_horn
+sudo useradd tuba
+sudo useradd timpani
+sudo useradd snaredrum
+sudo useradd bassdrum
+sudo useradd saxophone
+sudo useradd drumkit
+sudo usermod -aG sudo oboe
+sudo usermod -aG sudo drumkit
+sudo groupadd string
+sudo groupadd woodwind
+sudo groupadd percussion
+sudo groupadd brass
+sudo usermod -aG woodwind bassoon
+sudo usermod -aG woodwind oboe
+sudo usermod -aG brass trumpet
+sudo usermod -aG brass trombone
+sudo usermod -aG brass tuba
+sudo usermod -aG brass french_horn
+sudo usermod -aG string viola
+sudo usermod -aG string saxophone
+
+
 
